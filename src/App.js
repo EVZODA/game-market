a1bcc1e9dc0d5efaf71ccf10692dffa1b0c6a126
Xen0mrefne <x3n0g3n1c@gmail.com>
2022-09-09 11:28:38 -0300

    headlessui and heroicons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import './css/tailwind.css';
 import { Button } from 'flowbite-react'
-import { HiShoppingCart } from 'flowbite-react'
-import { HiOutlineArrowRight } from 'flowbite-react'
+import { ShoppingCartIcon } from '@heroicons/react/24/outline'
+import { ArrowRightIcon } from '@heroicons/react/24/outline'
 
 
 function App() {
   return (
     <>
-      <section className='main-section'></section>
-      <div className="flex flex-wrap items-center gap-2">
-        <Button>
-          <HiShoppingCart className="mr-2 h-5 w-5" />
-          Buy now
-        </Button>
-        <Button>
-          Choose plan
-          <HiOutlineArrowRight className="ml-2 h-5 w-5" />
-        </Button>
-      </div>
+    
     </>
   );
 }
